25d6505fc9552b2894901b77250d895f2616546c
sutengfei <736643403@qq.com>
2024-11-28 19:34:50 +0800

    add search and tables

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -2,7 +2,7 @@
  * @Author: sutengfei
  * @Date: 2024-11-24 19:39:16
  * @LastEditors: sutengfei
- * @LastEditTime: 2024-11-28 17:19:49
+ * @LastEditTime: 2024-11-28 18:40:32
  */
 import Pagination from "@/app/ui/invoices/pagination";
 import Search from "@/app/ui/search";
@@ -11,8 +11,19 @@ import { CreateInvoice } from "@/app/ui/invoices/buttons";
 import { lusitana } from "@/app/ui/fonts";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
+import { fetchInvoicesPages } from "@/app/lib/data";
 
-export default async function Page() {
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchInvoicesPages(query);
+
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
@@ -22,11 +33,11 @@ export default async function Page() {
         <Search placeholder="Search invoices..." />
         <CreateInvoice />
       </div>
-      {/*  <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
-      </Suspense> */}
+      </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
