cbcaa6f83fcf7163b8085d5435a06f72ca5fdd01
sutengfei <736643403@qq.com>
2024-11-29 11:13:41 +0800

    mutate data using forms and react server actions

---
+++ app/dashboard/invoices/create/page.tsx
@@ -0,0 +1,29 @@
+/*
+ * @Author: sutengfei
+ * @Date: 2024-11-28 19:53:31
+ * @LastEditors: sutengfei
+ * @LastEditTime: 2024-11-28 19:54:06
+ */
+import Form from "@/app/ui/invoices/create-form";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import { fetchCustomers } from "@/app/lib/data";
+
+export default async function Page() {
+  const customers = await fetchCustomers();
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Invoices", href: "/dashboard/invoices" },
+          {
+            label: "Create Invoice",
+            href: "/dashboard/invoices/create",
+            active: true,
+          },
+        ]}
+      />
+      <Form customers={customers} />
+    </main>
+  );
+}
